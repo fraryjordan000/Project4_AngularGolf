4cad4c38e50fa338b75e738afdbac4c2b0918b91
JordanMac <fraryjordan000@gmail.com>
2018-12-28 16:12:12 -0700

    Groundwork started

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewGameComponent } from './new-game/new-game.component';
+import { GameComponent } from './game/game.component';
+
+const myRoutes: Routes = [
+  { path: "new-game" , component: NewGameComponent },
+  { path: "game", component: GameComponent},
+  { path: "", component: NewGameComponent},
+  { path: "**", component: NewGameComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(
+      myRoutes
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
