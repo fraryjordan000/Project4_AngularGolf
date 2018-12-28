8d0a8912fb846c6d5ef5c8fafcea43ff4b0e064d
JordanMac <fraryjordan000@gmail.com>
2018-12-28 16:42:45 -0700

    Routing finally works

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { NewGameComponent } from './new-game/new-game.component';
+import { GameComponent } from './game/game.component';
+
+const routes: Routes = [
+  { path: "new-game" , component: NewGameComponent },
+  { path: "game", component: GameComponent},
+  { path: "", redirectTo: "/new-game", pathMatch: "full"},
+  { path: "**", component: NewGameComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NewGameComponent } from './new-game/new-game.component';
 import { GameComponent } from './game/game.component';
 
-const myRoutes: Routes = [
-  { path: "new-game" , component: NewGameComponent },
-  { path: "game", component: GameComponent},
-  { path: "", component: NewGameComponent},
-  { path: "**", component: NewGameComponent }
-];
-
 @NgModule({
   declarations: [
     AppComponent,
+    NewGameComponent,
     GameComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    RouterModule.forRoot(
-      myRoutes
-    )
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
